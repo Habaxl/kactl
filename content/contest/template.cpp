680ae9665c0ef922245875ff6a78cc00b957351a
Habaxl <107802078+Habaxl@users.noreply.github.com>
2025-11-07 21:24:29 +0700

    Update template.cpp

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,33 +1,14 @@
-// #pragma GCC optimize("Ofast")
-// #pragma GCC target("avx,avx2,fma")
 #include <bits/stdc++.h>
-#define freopen(name) if(fopen(name".INP","r")) {freopen (name".INP","r",stdin); freopen (name".OUT","w",stdout);}
 using namespace std;
 
-using ll = long long;
-
-void justDoIt();
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
 
 int main() {
-    // freopen("");
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    justDoIt();
-    return 0;
-}
-
-const int N = 1;
-
-
-
-void test() {
-    
-}
-
-void justDoIt() {
-    int t = 1;
-    // cin >> t;
-    for (int tests = 1; tests <= t; tests++) {
-        test();
-    }
+	cin.tie(0)->sync_with_stdio(0);
+	cin.exceptions(cin.failbit);
 }
